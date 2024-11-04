f711473b9299ad527aa6d51742437ee5f6d86f0e
NIU1633672 <93122170+NIU1633672@users.noreply.github.com>
2024-11-04 11:33:17 +0100

    Update game_controller.py
    Actualizado fichero controlador

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -1,1 +1,10 @@
+# src/controller/game_controller.py
+class GameController:
+    def __init__(self, model, view):
+        self.model = model  # Referencia al GameBoard
+        self.view = view    # Referencia al GameView
+
+    def move_left(self):
+        # Lógica para mover a la izquierda y actualizar la vista
+        pass
 
